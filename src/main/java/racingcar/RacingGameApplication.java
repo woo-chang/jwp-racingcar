2fa6d66e99b7bbfddd7ddf1902b459fa0b8931ab
woo-chang <clllickme@naver.com>
2023-04-11 22:37:43 +0900

    feat: 스프링 부트 애플리케이션 구현

--- src/main/java/racingcar/RacingGameApplication.java
+++ src/main/java/racingcar/RacingGameApplication.java
@@ -1,13 +1,17 @@
 package racingcar;
 
 import java.util.Scanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import racingcar.controller.RacingGameController;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
+@SpringBootApplication
 public class RacingGameApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(RacingGameApplication.class, args);
         final RacingGameController racingGameController = new RacingGameController(inputView(), outputView());
         racingGameController.run();
     }
